c63e370c4b13bf95b21a59cffb6943c26c0adc85
Nick Niles <nickyniles@gmail.com>
2015-08-11 23:20:22 +0200

    added ci colors, start on home design

--- app/js/app.js
+++ app/js/app.js
@@ -23,52 +23,52 @@ var app = (function(document, $) {
 	app.init();
 })();
 
-if (Modernizr.touch) { 
-    $('html').addClass('touch');
-} else { 
-    $('html').addClass('no-touch');
-}
+// if (Modernizr.touch) { 
+//     $('html').addClass('touch');
+// } else { 
+//     $('html').addClass('no-touch');
+// }
 
 $('.thumb-image').each(function(){
 	var myBG = 'img/thumbs/'+$(this).data('bg')+'.jpg';
 	console.log('myBG: ' + myBG);
-	$(this).css('background-image','url(' + myBG + ')')
+	$(this).css('background-image','url(' + myBG + ')');
 });
 
 $('.topic-image').each(function(){
 	var myBG = 'img/topics/'+$(this).data('bg')+'.jpg';
 	console.log('myBG: ' + myBG);
-	$(this).css('background-image','url(' + myBG + ')')
+	$(this).css('background-image','url(' + myBG + ')');
 });
 
 $('.publisher-image').each(function(){
 	var myBG = 'img/publishers/'+$(this).data('bg')+'.png';
 	console.log('myBG: ' + myBG);
-	$(this).css('background-image','url(' + myBG + ')')
+	$(this).css('background-image','url(' + myBG + ')');
 });
 
 
-var imageWidth = "test";
+var imageWidth = 'test';
 
-$('#image-width').html(imageWidth)
+$('#image-width').html(imageWidth);
 
 
 $(window).on('resize', function(){
       var imageWidth = $( window ).width() / 10;
       var factor = 100 - ($( window ).width() / 1920)*100;
-      console.log('factor: '+factor)
-      $('#image-width').html(imageWidth)
+      console.log('factor: '+factor);
+      $('#image-width').html(imageWidth);
  
 });
 
 $('.thumb-listenlater').on('click',function(){
-	$('.thumb-listenlater:after').css('color','#ff0000')
-	$(this).toggleClass('selected')
+	$('.thumb-listenlater:after').css('color','#ff0000');
+	$(this).toggleClass('selected');
 });
 
 $('.sortby .button').on('click',function(){
 	$('.sortby .button').each(function(){
-		$(this).removeClass('selected')
+		$(this).removeClass('selected');
 	});
-	$(this).addClass('selected')
+	$(this).addClass('selected');
 });
