000b7601cdf27a36a900ef7ff04fac0279256c25
Nick Niles <nickyniles@gmail.com>
2015-08-26 16:22:32 +0200

    published latest changes

--- app/js/sup-topbar.js
+++ app/js/sup-topbar.js
@@ -2,37 +2,37 @@
 // SlideUp for Foundation top-bar
 // ---------------------------------------------------------------
 
-console.log('whats sup topbar?');
+// console.log('whats sup topbar?');
 
-var didScroll;
-var lastScrollTop = 0;
-var scrollAmount = 10;          // Value of scroll amount
-var navbarHeight = $('.slideUp').outerHeight();
+// var didScroll;
+// var lastScrollTop = 0;
+// var scrollAmount = 10;          // Value of scroll amount
+// var navbarHeight = $('.slideUp').outerHeight();
 
-$(window).scroll(function(event){
-    didScroll = true;
-    console.log('didscroll?: '+ didScroll);
-});
+// $(window).scroll(function(event){
+//     didScroll = true;
+//     console.log('didscroll?: '+ didScroll);
+// });
 
-setInterval(function() {
-    if (didScroll) {
-        hasScrolled();
-        didScroll = false;
-    }
-}, 250);
+// setInterval(function() {
+//     if (didScroll) {
+//         hasScrolled();
+//         didScroll = false;
+//     }
+// }, 250);
 
-function hasScrolled() {
-    var sup = $(this).scrollTop();
+// function hasScrolled() {
+//     var sup = $(this).scrollTop();
 
-    if (sup > lastScrollTop && sup > navbarHeight){
-        // On Scroll Down
-        $('.slideUp').css({top: -$(this).outerHeight()});
-    } else {
-        // On Scroll Up
-        if(sup + $(window).height() < $(document).height()) {
-            $('.slideUp').css({top: 0});
-        }
-    }
+//     if (sup > lastScrollTop && sup > navbarHeight){
+//         // On Scroll Down
+//         $('.slideUp').css({top: -$(this).outerHeight()});
+//     } else {
+//         // On Scroll Up
+//         if(sup + $(window).height() < $(document).height()) {
+//             $('.slideUp').css({top: 0});
+//         }
+//     }
 
-    lastScrollTop = sup;
-}
+//     lastScrollTop = sup;
+// }
